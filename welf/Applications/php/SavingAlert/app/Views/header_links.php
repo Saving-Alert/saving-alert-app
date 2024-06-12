80bd0585defc5d1336a3dd1f71d17e635450e004
Thilina <prem.tsan@gmail.com>
2024-06-12 18:00:47 +0530

    Add: New header links

--- welf/Applications/php/SavingAlert/app/Views/header_links.php
+++ welf/Applications/php/SavingAlert/app/Views/header_links.php
@@ -4,9 +4,61 @@
         <!-- Level Two-->
     </li>
     <li>
-        <!-- <a href="<?php //echo base_url() ?>/Donations"><span class="title">Donations</span></a> -->
-
+        <a href="<?php echo base_url() ?>/Donations"><span class="title">Donations</span></a>
         <!-- Level Two-->
+    </li>
+
+    <li>
+        <a href="<?php echo base_url() ?>/Plasma"><span class="title">Plasma</span></a>
+        <ul>
+            <li><a href="#">What is plasma?</a></li>
+            <li><a href="#">Who can donate plasma</a>
+                <ul>
+                    <li><a href="#"><span class="title">Plasma donors and disability</span></a></li>
+                </ul>
+            </li>
+            <li><a href="#">Where to donate plasma</a></li>
+            <li><a href="#">How to donate plasma</a></li>
+            <li><a href="#">Why we need plasma donors</a></li>
+
+        </ul>
+    </li>
+
+    <li>
+        <a href="<?php echo base_url() ?>/WhyGiveBlood"><span class="title">Why give blood</span></a>
+        <ul>
+            <li><a href="#">Demand for different blood types</a></li>
+            <li><a href="#">Blood types</a></li>
+            <li><a href="#">How blood is used</a></li>
+            <li><a href="#">Who you could help</a></li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="<?php echo base_url() ?>/WhoCanGive"><span class="title">Who can give blood</span></a>
+        <ul>
+            <li><a href="#">Can I give blood?</a></li>
+            <li><a href="#">Getting an appointment</a>
+            <li><a href="#">Health and Eligibility</a></li>
+            <li><a href="#">Travel considerations</a></li>
+            <li><a href="#">Occupation considerations</a></li>
+            <li><a href="#">Donors and disability</a></li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="<?php echo base_url() ?>/WhoCanGive"><span class="title">The donation process</span></a>
+        <ul>
+            <li><a href="#">Giving blood for the first time</a></li>
+            <li><a href="#">Registering online</a>
+            <li><a href="#">Preparing to give blood</a></li>
+            <li><a href="#">What happens on the day</a></li>
+            <li><a href="#">After your donation</a></li>
+            <li><a href="#">About our donation venues</a></li>
+            <li><a href="#">Children at donation venues</a></li>
+            <li><a href="#">Further information</a></li>
+            <li><a href="#">Recognising donors</a></li>
+        </ul>
     </li>
 
     <li>
