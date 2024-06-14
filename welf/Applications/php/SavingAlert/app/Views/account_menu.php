cc2e4d9413431aa89e1cb48a8cc14284b8af8d57
Thilina <prem.tsan@gmail.com>
2024-06-15 02:15:11 +0530

    Add: Tracking menu specification

--- welf/Applications/php/SavingAlert/app/Views/account_menu.php
+++ welf/Applications/php/SavingAlert/app/Views/account_menu.php
@@ -10,7 +10,12 @@
 
 
     <li class="list-group-item"> <a href="<?php echo base_url(); ?>/Manage"> Manage </a> </li>
-    <li class="list-group-item"> <a href="<?php echo base_url(); ?>/Tracking"> Tracking </a> </li>
+        <?php
+        if(ami_reciver()){
+            echo '<li class="list-group-item"> <a href="'.base_url().'/Tracking">Tracking</a> </li> ';
+        }
+
+        ?>
     <li class="list-group-item"><a href="<?php echo base_url(); ?>/Profile"> Profile </a></li>
     <li class="list-group-item"><a href="<?php echo base_url(); ?>/Location"> Location </a></li>
 </ul>
